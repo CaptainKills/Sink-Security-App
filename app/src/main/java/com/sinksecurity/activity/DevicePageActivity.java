012bdbf13bbda4709032c56e23d098a580413554
CaptainKills <danick@triantis.nl>
2020-10-07 21:28:08 +0200

    Device Info Page is now more fuctional

--- app/src/main/java/com/sinksecurity/activity/DevicePageActivity.java
+++ app/src/main/java/com/sinksecurity/activity/DevicePageActivity.java
@@ -2,6 +2,7 @@ package com.sinksecurity.activity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.ActionBar;
@@ -23,7 +24,7 @@ public class DevicePageActivity extends AppCompatActivity {
     private RequestQueue requestQueue;
     private SinkSecurityDevice clickedDevice;
 
-    private TextView text;
+    private TextView statusText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +36,7 @@ public class DevicePageActivity extends AppCompatActivity {
             actionBar.setTitle(R.string.title_device_page);
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
-        
+
         clickedDevice = getIntent().getParcelableExtra("SinkSecurityDevice");
         setPageContents();
         createRequestQueue();
@@ -45,8 +46,17 @@ public class DevicePageActivity extends AppCompatActivity {
      * Configures all page contents with the SinkSecurityDevice information
      */
     private void setPageContents(){
-        text = findViewById(R.id.version_textView);
-        text.setText("Checking status...");
+        ImageView imageView = findViewById(R.id.page_device_icon);
+        imageView.setImageResource(clickedDevice.getDeviceImage());
+
+        TextView deviceNameView = findViewById(R.id.page_device_name);
+        deviceNameView.setText(clickedDevice.getName());
+
+        statusText = findViewById(R.id.page_device_status);
+        statusText.setText(R.string.device_status_check_text);
+
+        TextView deviceIpView = findViewById(R.id.page_device_ip);
+        deviceIpView.setText(clickedDevice.getIp());
     }
 
     private void createRequestQueue(){
@@ -57,13 +67,29 @@ public class DevicePageActivity extends AppCompatActivity {
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
-                text.setText(response);
                 System.out.println("Request: " + response);
+                switch (response){
+                    case "LEVEL_0":
+                        statusText.setText(R.string.status_level_0);
+                        break;
+                    case "LEVEL_1":
+                        statusText.setText(R.string.status_level_1);
+                        break;
+                    case "LEVEL_2":
+                        statusText.setText(R.string.status_level_2);
+                        break;
+                    case "LEVEL_3":
+                        statusText.setText(R.string.status_level_3);
+                        break;
+                    default:
+                        statusText.setText(R.string.device_status_error_text);
+                        break;
+                }
             }
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                text.setText(error.toString());
+                statusText.setText(R.string.device_status_failed_text);
                 System.out.println("Request error: " + error.toString());
             }
         });
